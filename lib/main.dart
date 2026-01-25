23e90deb23bd2b87066d374bfca4c7875597d2a1
Reece Cussons <reececussons@gmail.com>
2026-01-25 20:54:37 +0200

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'config/routes/router.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
+
+runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final router = ref.watch(goRouterProvider);
+@override
+Widget build(BuildContext context, WidgetRef ref) {
+final router = ref.watch(goRouterProvider);
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-    );
-  }
+return MaterialApp.router(
+debugShowCheckedModeBanner: false,
+routerConfig: router,
+);
+}
 }
